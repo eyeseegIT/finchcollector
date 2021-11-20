b5ee60860f676442b3745fb4bcd203b17d3810e7
Angela Quach <angelayunquach@gmail.com>
2021-11-20 15:36:08 -0500

    finish part 5

--- main_app/urls.py
+++ main_app/urls.py
@@ -17,5 +17,15 @@ urlpatterns = [
   path("finches/<int:pk>/delete", views.FinchDelete.as_view(), name="finches_delete"),
 
   # DO WE USE <int:finch_id> here (instead of pk) b/c it's not a CBV?
-  path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding')
+  path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+
+  path("toys/create", views.ToyCreate.as_view(), name="toys_create"),
+
+  path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+  
+  path('toys/', views.ToyList.as_view(), name='toys_index'),
+
+  path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+  
+  path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from .models import Finch, Toy
 from .forms import FeedingForm
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -40,3 +41,21 @@ def add_feeding(request, finch_id):
     new_feeding.finch_id = finch_id
     new_feeding.save()
   return redirect('finches_detail', finch_id=finch_id)
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = "__all__"
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
