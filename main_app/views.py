d527413dfb2a45645c0524fe73a811563ad07e3c
Angela Quach <angelayunquach@gmail.com>
2021-11-19 21:42:49 -0500

    finish part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+finches = []
+
 # Create your views here.
 
 def home(request):
   return HttpResponse("<h1>Hello!</h1>")
 
 def about(request):
-  return HttpResponse("<h1>About the FinchCollector</h1>")
+  return render(request, 'about.html')
+
+class Finch:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+def finches_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
