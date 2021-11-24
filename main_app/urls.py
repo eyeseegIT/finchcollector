172386edfe61a6d768e52166b6acdb884f3f839b
Angela Quach <angelayunquach@gmail.com>
2021-11-23 21:29:01 -0500

    finish part 6

--- main_app/urls.py
+++ main_app/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
   
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+
+  path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
 ]
